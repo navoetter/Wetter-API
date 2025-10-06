9e7ee3a9624a394d1a5b393e75a5f8d01984c1c4
navoetter <navoetter@tsn.at>
2025-10-06 12:29:48 +0200

    Key noch nicht aktiv

---
+++ api-probe.py
@@ -0,0 +1,11 @@
+import requests
+
+api_key = "92976e470f54076469aa87b480cdb07f"  
+city = "Wien"
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+print(data)

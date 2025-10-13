b8d68e2b961a5a43538a1935ef15149fc1687108
navoetter <navoetter@tsn.at>
2025-10-13 10:55:43 +0200

    API-Probe

--- api-probe.py
+++ api-probe.py
@@ -3,7 +3,7 @@ import requests
 api_key = "92976e470f54076469aa87b480cdb07f"  
 city = "Wien"
 
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
 response = requests.get(url)
 data = response.json()

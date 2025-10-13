248b61def0d12ea9337b02a5bfe1e847de55da79
navoetter <navoetter@tsn.at>
2025-10-13 11:01:24 +0200

    Eingabe

---
+++ Eingabe-Probe.py
@@ -0,0 +1,18 @@
+import requests
+
+api_key = "92976e470f54076469aa87b480cdb07f"  
+
+city = input("Gib den Ort ein: ").strip()
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data["cod"] != 200:
+    print("❌ Stadt nicht gefunden!")
+else:
+    print(f"Stadt: {data['name']}")
+    print(f"Temperatur: {data['main']['temp']} °C")
+    print(f"Wetter: {data['weather'][0]['description']}")
+    print(f"Luftfeuchtigkeit: {data['main']['humidity']}%")
